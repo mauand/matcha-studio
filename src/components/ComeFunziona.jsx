b460f1de851927db0590024651c17ceda15b08c1
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-24 15:53:27 +0400

    Adedd come funziona

---
+++ src/components/ComeFunziona.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComeFunziona = () => {
+  return (
+    <div className='px-6'>
+        <h5>Come funziona</h5>
+        <h2>È facile e veloce...</h2>
+    </div>
+  )
+}
+
+export default ComeFunziona
