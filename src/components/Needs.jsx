60f1e6b5027637dff802f80f875c72a7084781c4
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-24 20:02:35 +0400

    Finish needs

--- src/components/Needs.jsx
+++ src/components/Needs.jsx
@@ -4,6 +4,24 @@ const Needs = () => {
   return (
     <div className='px-4'>
         <h1>Di cosa hai bisogno</h1>
+        <div className="flex flex-col lg:flex-row text-center gap-12">
+        <div>
+                <img src="/matcha-studio/1yogi.png" alt="yogi uno" />
+                <h4 className='uppercase'>IL DESIDERIO DI SENTIRTI BENE</h4>
+            </div>
+            <div>
+                <img src="/matcha-studio/2yogi.png" alt="yogi uno" />
+                <h4 className='uppercase'>UN PICCOLO SPAZIO TRANQUILLO</h4>
+            </div>
+            <div>
+                <img src="/matcha-studio/3yogi.png" alt="yogi uno" />
+                <h4 className='uppercase'>UN MOMENTO PER  IL TUO BENESSERE</h4>
+            </div>
+            <div>
+                <img src="/matcha-studio/4yogi.png" alt="yogi uno" />
+                <h4 className='uppercase'>UN DISPOSITIVO DAL QUALE CONETTERTI</h4>
+            </div>
+        </div>
     </div>
   )
 }
