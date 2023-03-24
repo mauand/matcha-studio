a8656900c59811624f3488a731e5f29ebfe430a3
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-24 20:04:36 +0400

    Fix

--- src/components/Needs.jsx
+++ src/components/Needs.jsx
@@ -6,19 +6,19 @@ const Needs = () => {
         <h1>Di cosa hai bisogno</h1>
         <div className="flex flex-col lg:flex-row text-center gap-12">
         <div>
-                <img src="/matcha-studio/1yogi.png" alt="yogi uno" />
+                <img src="/matcha-studio/1yogi.png" alt="yogi uno" className='w-4/5 mx-auto'/>
                 <h4 className='uppercase'>IL DESIDERIO DI SENTIRTI BENE</h4>
             </div>
             <div>
-                <img src="/matcha-studio/2yogi.png" alt="yogi uno" />
+                <img src="/matcha-studio/2yogi.png" alt="yogi uno" className='w-4/5 mx-auto'/>
                 <h4 className='uppercase'>UN PICCOLO SPAZIO TRANQUILLO</h4>
             </div>
             <div>
-                <img src="/matcha-studio/3yogi.png" alt="yogi uno" />
+                <img src="/matcha-studio/3yogi.png" alt="yogi uno" className='w-4/5 mx-auto'/>
                 <h4 className='uppercase'>UN MOMENTO PER  IL TUO BENESSERE</h4>
             </div>
             <div>
-                <img src="/matcha-studio/4yogi.png" alt="yogi uno" />
+                <img src="/matcha-studio/4yogi.png" alt="yogi uno" className='w-4/5 mx-auto'/>
                 <h4 className='uppercase'>UN DISPOSITIVO DAL QUALE CONETTERTI</h4>
             </div>
         </div>
