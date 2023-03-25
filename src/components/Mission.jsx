dfd1792e8ef4f436e2f709ea2abfa3fa13aa60f5
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-25 15:25:37 +0400

    Trying some color

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -3,11 +3,11 @@ import Button from "./Button";
 
 const Description = () => {
   return (
-    <div className="px-0 rounded-md bg-[#566743]">
+    <div className="px-0 rounded-md bg-[#6B8351]">
       <div className="px-4 py-16">
-        <h3 className="uppercase mb-4 text-gray-100">La nostra <br />mission</h3>
+        <h3 className="uppercase mb-4 text-[#F8AA71]">La nostra <br />mission</h3>
         <p className="mb-8 text-gray-100">Questo progetto nasce con l’intento di dare la possibilità davvero a tutti di potersi prendere cura di sé da ogni luogo e in ogni momento disponibile. </p>
-        <h2 className="mb-4 text-[#F8AA71]">Pratico e accessibile </h2>
+        <h2 className="mb-4">Pratico e accessibile </h2>
         <p className="mb-8 text-gray-100">Matcha Studio costa quanto un caffè del mattino, ed è aperto H24/7!<br /><br />
 Le lezioni sono LIVE ma rimangono registrate per quando non riesci ad essere presente.<br /><br />
 Matcha Studio ha insegnanti competenti sempre a tua disposizione e ti riserva una consulenza gratuita per capire quale è il corso più adatto a te.<br /><br />
