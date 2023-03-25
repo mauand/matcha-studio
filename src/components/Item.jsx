08be61ca99787f68a96bd5dbe8cd6d86c6fb78d4
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-25 16:44:14 +0400

    Fic

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Item = ({ name, className }) => {
   return (
-    <div className={`w-full py-8 px-6 bg-white rounded-md text-center mb-2 ${className}`}>
+    <div className={`w-full py-8 px-6 rounded-md text-center mb-2 ${className}`}>
       <h5>{name}</h5>
     </div>
   )
