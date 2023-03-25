cbf82723ec14b346198b2013e6d373e9653dbd95
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-25 14:02:06 +0400

    Button fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ title }) => {
   return (
-    <button className="w-full py-4 bg-green-600 text-white font-semibold rounded-md uppercase">
+    <button className="w-full py-4 bg-green-600 text-white font-bold rounded-md text-sm uppercase">
       {title}
     </button>
   );
