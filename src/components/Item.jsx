31bdf0706ecd93c3f8213e69a05b996915ccc4f1
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-28 23:50:12 +0400

    Some responsive design changes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Item = ({ name, className }) => {
   return (
     <div
-      className={`w-full h-28 rounded-xl text-center ${className} bg-center bg-cover`}
+      className={`w-full h-28 md:h-32 rounded-xl text-center ${className} bg-center bg-cover`}
     >
       <div className="h-full w-full py-8 px-6 rounded-xl backdrop-brightness-[.6] flex justify-center items-center">
         <h5 className="title-shadow">{name}</h5>
