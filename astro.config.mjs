55b0dc075914448ee4ca96cdd0c1ae781f6beabb
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-31 01:00:43 +0400

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
-
 import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://matchastudioyoga.com',
-  
   integrations: [tailwind(), react(), partytown({
     config: {
       forward: ["dataLayer.push"]
     }
-  }), robotsTxt()],
+  }), robotsTxt(), sitemap()]
 });
