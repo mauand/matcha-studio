59e40e28d7510e28a1fce3d4f1cdf7d65c53fd4b
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-31 00:56:18 +0400

    first build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import partytown from "@astrojs/partytown";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://matchastudioyoga.com',
+  
   integrations: [tailwind(), react(), partytown({
-    config: { 
-      forward: ["dataLayer.push"] 
-    },
-  })],
-  site: 'https://matchastudioyoga.com'
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), robotsTxt()],
 });
