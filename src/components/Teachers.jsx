26bd2ea4fd048a80b75a1d435327dda673e304f9
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-31 18:42:58 +0400

    change scroll plugin

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -1,26 +1,22 @@
-import React, { useRef } from "react";
 import Teacher from "./Teach";
-
+import React from "react";
 
 const Teachers = () => {
-
-
   return (
     <div className="px-4">
-      <div
-
-        className="flex flex-nowrap gap-4 overflow-x-scroll no-scrollbar cursor-grab unselectable rounded-xl"
-      >
+      <div className="flex flex-nowrap gap-4 overflow-x-scroll no-scrollbar cursor-grab unselectable rounded-xl">
         <Teacher
           name="Angelica Albertini"
           desc="Insegnante di Yoga e Meditazione"
           className="bg-[url('https://ik.imagekit.io/mauand/tr:w-500/matcha-studio/item2.jpg')] text-white"
         />
+
         <Teacher
           name="Francesca"
           desc="Insegnante di Yoga e Meditazione"
           className="bg-[url('https://ik.imagekit.io/mauand/tr:w-500/matcha-studio/item2.jpg')] text-white"
         />
+
         <Teacher
           name="Angelica Albertini"
           desc="Insegnante di Yoga e Meditazione"
