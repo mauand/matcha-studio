ae94906fa7589d74e95bb31fd849daad708239a0
MauAnd <am.andreoli.mauro@gmail.com>
2023-03-31 18:57:33 +0400

    change scroll plugin

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -1,29 +1,31 @@
 import Teacher from "./Teach";
 import React from "react";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 const Teachers = () => {
+
   return (
-    <div className="px-4">
-      <div className="flex flex-nowrap gap-4 overflow-x-scroll no-scrollbar cursor-grab unselectable rounded-xl">
-        <Teacher
-          name="Angelica Albertini"
-          desc="Insegnante di Yoga e Meditazione"
-          className="bg-[url('https://ik.imagekit.io/mauand/tr:w-500/matcha-studio/item2.jpg')] text-white"
-        />
+      <ScrollContainer>
+        <div className="flex flex-nowrap gap-4 unselectable rounded-xl">
+          <Teacher
+            name="Angelica Albertini"
+            desc="Insegnante di Yoga e Meditazione"
+            className="bg-[url('https://ik.imagekit.io/mauand/tr:w-500/matcha-studio/item2.jpg')] text-white"
+          />
 
-        <Teacher
-          name="Francesca"
-          desc="Insegnante di Yoga e Meditazione"
-          className="bg-[url('https://ik.imagekit.io/mauand/tr:w-500/matcha-studio/item2.jpg')] text-white"
-        />
+          <Teacher
+            name="Francesca"
+            desc="Insegnante di Yoga e Meditazione"
+            className="bg-[url('https://ik.imagekit.io/mauand/tr:w-500/matcha-studio/item2.jpg')] text-white"
+          />
 
-        <Teacher
-          name="Angelica Albertini"
-          desc="Insegnante di Yoga e Meditazione"
-          className="bg-[url('https://ik.imagekit.io/mauand/tr:w-500/matcha-studio/item2.jpg')] text-white"
-        />
-      </div>
-    </div>
+          <Teacher
+            name="Angelica Albertini"
+            desc="Insegnante di Yoga e Meditazione"
+            className="bg-[url('https://ik.imagekit.io/mauand/tr:w-500/matcha-studio/item2.jpg')] text-white"
+          />
+        </div>
+      </ScrollContainer>
   );
 };
 
