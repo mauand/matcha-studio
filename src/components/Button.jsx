844d20d3fef1958ff55effb4ab86caca8eb5e1e6
MauAnd <am.andreoli.mauro@gmail.com>
2023-04-01 16:28:19 +0400

    link now open in a new tab

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ title, className, link }) => {
   return (
-    <a href={link} className="w-full max-w-[22rem]">
+    <a href={link} className="w-full max-w-[22rem]"  target="_blank">
     <button 
       className={`w-full max-w-[22rem] text-center py-4 font-bold rounded-md text-sm uppercase shadow-md transition-all ${className}`}
     >
