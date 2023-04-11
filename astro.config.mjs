75e3c307b35d3f25f673542dbfe551302017ccd3
MauAnd <am.andreoli.mauro@gmail.com>
2023-04-12 01:33:54 +0400

    fixed facebook pixel

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,14 @@ export default defineConfig({
   site: 'https://matchastudioyoga.com',
   integrations: [tailwind(), react(), partytown({
     config: {
+      resolveUrl={function(url) {
+        if (url.hostname === "connect.facebook.net") {
+          var proxyUrl = new URL('https://my-reverse-proxy.com/');
+          proxyUrl.searchParams.append('url', url.href);
+          return proxyUrl;
+        }
+        return url;
+      },
       forward: ["dataLayer.push", "fbq"]
     }
   }), robotsTxt(), sitemap()]
