6103ff1b8cf8c9f84ac5fecaf10941e51cf9b034
MauAnd <am.andreoli.mauro@gmail.com>
2023-04-16 17:16:37 +0400

    fixed facebook pixel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
 
 import sitemap from "@astrojs/sitemap";
@@ -9,9 +8,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://matchastudioyoga.com',
-  integrations: [tailwind(), react(), partytown({
-    config: {
-        "forward": ["dataLayer.push", "fbq"],
-      },
-  }),, robotsTxt(), sitemap()]
+  integrations: [tailwind(), react(), robotsTxt(), sitemap()]
 });
